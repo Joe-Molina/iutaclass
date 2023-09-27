3afd847b6f8cfdc2fbee148cfc172db3d216b6fa
joe <joedodaniljr123@gmail.com>
2023-09-27 08:42:43 -0400

    cambie funciones por repetir el codigo y ya

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -6,14 +6,10 @@ export const getRegister = async (req, res) => {
   res.render("register", { error: false });
 };
 
-const funCrearUsuario = async (tipo) => {
-  // buscar el email en la base de datos para ver si existe
-  //si existe no permitir crear otro usuario
-  //si no existe crearlo
-  //redirigir al login
+export const crearEstudiante = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const userType_id = tipo;
+    const userType_id = "1";
     const user = await Users.findOne({
       where: { email },
     });
@@ -33,18 +29,71 @@ const funCrearUsuario = async (tipo) => {
   }
 };
 
-export const crearEstudiante = async (req, res) => {
-  funCrearUsuario("1");
-};
-
 export const crearDocente = async (req, res) => {
-  funCrearUsuario("2");
+  try {
+    const { name, email, password } = req.body;
+    const userType_id = "2";
+    const user = await Users.findOne({
+      where: { email },
+    });
+    if (!user) {
+      const newUser = await Users.create({
+        name,
+        email,
+        password,
+        userType_id,
+      });
+      res.json("usuario creado");
+    } else {
+      res.json("usuario ya registrado");
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 export const crearCoordinador = async (req, res) => {
-  funCrearUsuario("3");
+  try {
+    const { name, email, password } = req.body;
+    const userType_id = "3";
+    const user = await Users.findOne({
+      where: { email },
+    });
+    if (!user) {
+      const newUser = await Users.create({
+        name,
+        email,
+        password,
+        userType_id,
+      });
+      res.json("usuario creado");
+    } else {
+      res.json("usuario ya registrado");
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 export const crearSU = async (req, res) => {
-  funCrearUsuario("4");
+  try {
+    const { name, email, password } = req.body;
+    const userType_id = "4";
+    const user = await Users.findOne({
+      where: { email },
+    });
+    if (!user) {
+      const newUser = await Users.create({
+        name,
+        email,
+        password,
+        userType_id,
+      });
+      res.json("usuario creado");
+    } else {
+      res.json("usuario ya registrado");
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
